d46e9d3b8f2d8a99d417d85baba382860239c376
ccat <ccathlete01@gmail.com>
2025-02-24 18:18:29 +0100

    Mid implementation of crossover.

--- src/natural_events.py
+++ src/natural_events.py
@@ -1,6 +1,30 @@
 """
 Selection, Crossover and Mutation.
 """
-from definitions import *
-from population import Population
 
+from definitions import *
+from population import *
+
+
+def select(population: Population) -> tuple[Individual, ...]:
+    """
+    Tournament selection process.
+    """
+    return tuple(random.choices(population=population.individuals, k=2))
+
+
+def crossover(
+    parent1: Individual,
+    parent2: Individual,
+) -> tuple[Individual, Individual]:
+    """Single point crossover.
+
+    Returns:
+        tuple[Individual, Individual]: Two children.
+    """
+    # We need to take genes from both sides of the crossover point
+    # so we can't have the point in index 0 or len - 1.
+    crossover_point: int = random.randint(1, GENOME_LEN - 1)
+    # TODO: finish.
+
+    return (Individual([]), Individual([]))
