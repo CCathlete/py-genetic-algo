a5311fb08c77eef4049a3876334e0a2c2c98eab9
ccat <ccathlete01@gmail.com>
2025-02-24 21:05:29 +0100

    Implemented crossover and mutation.

--- src/natural_events.py
+++ src/natural_events.py
@@ -24,7 +24,21 @@ def crossover(
     """
     # We need to take genes from both sides of the crossover point
     # so we can't have the point in index 0 or len - 1.
-    crossover_point: int = random.randint(1, GENOME_LEN - 1)
-    # TODO: finish.
+    crossover_point: int = random.randint(1, GENOME_LEN - 2)
+    child1: Individual = Individual(
+        parent1.genome[:crossover_point] + parent2.genome[crossover_point:]
+    )
+    child2: Individual = Individual(
+        parent2.genome[:crossover_point] + parent1.genome[crossover_point:]
+    )
 
-    return (Individual([]), Individual([]))
+    return child1, child2
+
+
+def mutate(ind: Individual) -> Individual:
+    """
+    Random flip Mutation of one gene.
+    """
+    index: int = random.randint(0, GENOME_LEN - 1)
+    ind.genome[index] = not ind.genome[index]
+    return ind
