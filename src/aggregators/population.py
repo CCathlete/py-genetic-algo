24bb119c9bfd1f480309300aae291a44e0c7ace1
ccat <ccathlete01@gmail.com>
2025-02-25 15:37:54 +0100

    Changed Population to a regular class instead of a dataclass (dataclasses can't have mutable fields).

--- src/aggregators/population.py
+++ src/aggregators/population.py
@@ -5,18 +5,17 @@ Initialising the population.
 import random
 from src.definitions import *
 from src.entities.individual import Individual
-from dataclasses import dataclass
 
 
-@dataclass
 class Population:
     """
     A data structure that groups all of the individuals in the
     population.
     """
 
-    individuals: list[Individual] = [
-        # Creating a random genome for each individual.
-        Individual(genes=random.choices(population=[True, False], k=GENOME_LEN))
-        for _ in range(POPULATION_SIZE)
-    ]
+    def __init__(self):
+        self.individuals: list[Individual] = [
+            # Creating a random genome for each individual.
+            Individual(genes=random.choices(population=[True, False], k=GENOME_LEN))
+            for _ in range(POPULATION_SIZE)
+        ]
